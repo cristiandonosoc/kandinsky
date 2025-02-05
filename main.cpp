156c2d169b5fdfbcd110e0c5e94480b61e3a86e4
Cristián Donoso <cristiandonosoc@gmail.com>
2025-02-05 18:02:54 -0500

    [sdl] Open window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,49 @@
 #define SDL_MAIN_HANDLED
 #include <SDL3/SDL.h>
 
+static SDL_Window *gSDLWindow = nullptr;
+static bool gDone = false;
+
+static constexpr int kWidth = 1024;
+static constexpr int kHeight = 720;
+
+bool Update() {
+  SDL_Event e;
+  if (SDL_PollEvent(&e)) {
+    if (e.type == SDL_EVENT_QUIT) {
+      return false;
+    }
+
+    if (e.type == SDL_EVENT_KEY_UP) {
+      if (e.key.key == SDLK_ESCAPE) {
+        return false;
+      }
+    }
+  }
+
+  SDL_Delay(1);
+  return true;
+}
+
 int main() {
-    SDL_Log("%s", "Hello World!");
-    return 0;
+  if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
+    SDL_Log("ERROR: Initializing");
+    return -1;
+  }
+
+  gSDLWindow = SDL_CreateWindow("SDL3 window", kWidth, kHeight, 0);
+  if (!gSDLWindow) {
+    SDL_Log("ERROR: Window");
+    return -1;
+  }
+
+  while (!gDone) {
+    if (!Update()) {
+      break;
+    }
+  }
+
+  SDL_DestroyWindow(gSDLWindow);
+
+  return 0;
 }
