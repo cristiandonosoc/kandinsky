98e94b749b68297b1921c27324f3004877641f13
Cristián Donoso <cristiandonosoc@gmail.com>
2025-02-22 22:24:11 -0500

    Added glslangValidator and script to quickly validate shaders

---
+++ validate_shaders.bat
@@ -0,0 +1,17 @@
+@echo off
+@set SCRIPT_PATH=%~dp0
+@pushd %SCRIPT_PATH%
+
+set VALIDATOR=extras\glsl\glslangValidator.exe
+if not exist %VALIDATOR% (
+	echo Cannot find %VALIDATOR%
+	exit /b 1
+)
+echo Found validator at %VALIDATOR%
+
+@for %%f in (assets\shaders\*) do (
+	@echo Validating %%f
+	%VALIDATOR% %%f
+)
+
+
