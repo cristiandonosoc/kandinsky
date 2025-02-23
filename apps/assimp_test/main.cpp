f1739c5906c0bf4ddbe50fcdacfd5d26d8c55f1c
Cristián Donoso <cristiandonosoc@gmail.com>
2025-02-22 21:19:14 -0500

    Add precompiled assimp library to third_party

---
+++ apps/assimp_test/main.cpp
@@ -0,0 +1,36 @@
+#include <kandinsky/defines.h>
+
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
+#include <assimp/Importer.hpp>
+
+#include <SDL3/SDL.h>
+
+int main() {
+    Assimp::Importer importer;
+
+	const char* path = "D:/assets/Survival_BackPack_2/Survival_BackPack_2.fbx";
+	SDL_PathInfo info = {};
+	if (!SDL_GetPathInfo(path, &info)) {
+		SDL_Log("ERROR: GetPathInfo %s: %s\n", path, SDL_GetError());
+		return -1;
+	}
+
+
+    const aiScene* scene =
+        importer.ReadFile(path,
+                          aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_GenNormals |
+                              aiProcess_CalcTangentSpace);
+	if (!scene) {
+		SDL_Log("ERROR: Loading model at %s: %s\n", path, importer.GetErrorString());
+		return -1;
+	}
+
+	SDL_Log("Model at %s loaded!\n", path);
+
+	SDL_Log("Meshes: %d\n", scene->mNumMeshes);
+	for (u32 i = 0; i < scene->mNumMeshes; i++) {
+		aiMesh* mesh = scene->mMeshes[i];
+		SDL_Log("- %s\n", mesh->mName.C_Str());
+	}
+}
