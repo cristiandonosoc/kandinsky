f1c8dacecae220ff76dd85f3380ab9ec825fe58d
Cristián Donoso <cristiandonosoc@gmail.com>
2025-03-26 10:54:08 -0400

    Serde supports nested structs and types

--- apps/experimental/serde/serde.cpp
+++ apps/experimental/serde/serde.cpp
@@ -1,32 +1,12 @@
-#include <cstdio>
-
 #include <kandinsky/container.h>
 #include <kandinsky/math.h>
 #include <kandinsky/memory.h>
 #include <kandinsky/string.h>
 #include <yaml-cpp/yaml.h>
 
-#include <fstream>
 #include <iostream>
 
 namespace kdk {
-
-struct Bar {
-    String Name;
-
-    Transform Transform = {};
-
-    DynArray<int> Ints = {};
-    DynArray<String> Addresses = {};
-};
-
-struct Foo {
-    String Name = {};
-    int Age = 0;
-    float Height = 0.0f;
-
-    DynArray<Bar> Bars = {};
-};
 
 struct SerdeArchive {
     Arena* Arena = nullptr;
@@ -50,9 +30,11 @@ void Serde(SerdeArchive* ar, const char* name, T& t) {
     ar->CurrentNode = &node;
     Serialize(ar, t);
 
-    (*prev)[name] = node;
+    (*prev)[name] = std::move(node);
     ar->CurrentNode = prev;
 }
+
+#define SERDE(ar, owner, member) Serde(ar, #member, owner.member)
 
 template <>
 void Serde<String>(SerdeArchive* ar, const char* name, String& value) {
@@ -69,23 +51,31 @@ void Serde<float>(SerdeArchive* ar, const char* name, float& value) {
     (*ar->CurrentNode)[name] = value;
 }
 
-template <>
-void Serde<Vec3>(SerdeArchive* ar, const char* name, Vec3& value) {
-    YAML::Node node;
+void SerdeInline(YAML::Node& node, Vec3& value) {
     node["x"] = value.x;
     node["y"] = value.y;
     node["z"] = value.z;
-    (*ar->CurrentNode)[name] = node;
+}
+
+template <>
+void Serde<Vec3>(SerdeArchive* ar, const char* name, Vec3& value) {
+    YAML::Node node;
+    SerdeInline(node, value);
+    (*ar->CurrentNode)[name] = std::move(node);
+}
+
+void SerdeInline(YAML::Node& node, Quat& value) {
+    node["x"] = value.x;
+    node["y"] = value.y;
+    node["z"] = value.z;
+    node["w"] = value.w;
 }
 
 template <>
 void Serde<Quat>(SerdeArchive* ar, const char* name, Quat& value) {
     YAML::Node node;
-    node["x"] = value.x;
-    node["y"] = value.y;
-    node["z"] = value.z;
-    node["w"] = value.w;
-    (*ar->CurrentNode)[name] = node;
+    SerdeInline(node, value);
+    (*ar->CurrentNode)[name] = std::move(node);
 }
 
 template <>
@@ -94,52 +84,79 @@ void Serde<Transform>(SerdeArchive* ar, const char* name, Transform& value) {
 
     YAML::Node node;
     ar->CurrentNode = &node;
-    Serde(ar, "Position", value.Position);
-    Serde(ar, "Rotation", value.Rotation);
-    Serde(ar, "Scale", value.Scale);
+    SERDE(ar, value, Position);
+    SERDE(ar, value, Rotation);
+    SERDE(ar, value, Scale);
 
-    (*prev)[name] = node;
+    (*prev)[name] = std::move(node);
     ar->CurrentNode = prev;
 }
 
-void Serialize(SerdeArchive* ar, Bar& bar) {
-    YAML::Node node;
+template <typename T>
+concept HasInlineSerde = requires(T value) {
+    { SerdeInline(std::declval<YAML::Node&>(), value) };
+};
 
-    Serde(ar, "Name", bar.Name);
-    Serde(ar, "Transform", bar.Transform);
+template <typename T>
+void Serde(SerdeArchive* ar, const char* name, DynArray<T>& values) {
+    auto* prev = ar->CurrentNode;
+    YAML::Node array_node = YAML::Node(YAML::NodeType::Sequence);
 
-    // // Serialize integer array
-    // YAML::Node ints;
-    // for (u32 i = 0; i < bar.Ints.Size; ++i) {
-    //     ints.push_back(bar.Ints[i]);
-    // }
-    // node["ints"] = ints;
+    for (u32 i = 0; i < values.Size; i++) {
+        auto& value = values[i];
+        if constexpr (std::is_arithmetic_v<T>) {
+            array_node.push_back(value);
+        } else if constexpr (std::is_same_v<T, String>) {
+            array_node.push_back(value.Str());
+        } else if constexpr (HasInlineSerde<T>) {
+            YAML::Node node;
+            SerdeInline(node, value);
+            array_node.push_back(std::move(node));
+        } else {
+            YAML::Node node;
+            ar->CurrentNode = &node;
+            Serialize(ar, value);
+            array_node.push_back(std::move(node));
+        }
+    }
 
-    // // Serialize string array
-    // YAML::Node addresses;
-    // for (u32 i = 0; i < bar.Addresses.Size; ++i) {
-    //     addresses.push_back(bar.Addresses[i].Str());
-    // }
-    // node["addresses"] = addresses;
+    (*prev)[name] = std::move(array_node);
+    ar->CurrentNode = prev;
 }
+
+struct Bar {
+    String Name;
+
+    Transform Transform = {};
+
+    DynArray<String> Addresses = {};
+    DynArray<Vec3> Positions = {};
+};
+
+void Serialize(SerdeArchive* ar, Bar& bar) {
+    SERDE(ar, bar, Name);
+    SERDE(ar, bar, Transform);
+    SERDE(ar, bar, Addresses);
+    SERDE(ar, bar, Positions);
+}
+
+struct Foo {
+    String Name = {};
+    int Age = 0;
+    float Height = 0.0f;
+
+    DynArray<int> Ints = {};
+    DynArray<Bar> Bars = {};
+};
 
 void Serialize(SerdeArchive* ar, Foo& foo) {
     YAML::Node node;
 
-    Serde(ar, "Name", foo.Name);
-    Serde(ar, "Age", foo.Age);
-    Serde(ar, "Height", foo.Height);
-
-    // // Serialize Bar array
-    // YAML::Node bars;
-    // for (u32 i = 0; i < foo.Bars.Size; ++i) {
-    //     Serde barSerde;
-    //     Serialize(&barSerde, arena, foo.Bars[i]);
-    //     bars.push_back(barSerde.node);
-    // }
-    // node["bars"] = bars;
-
-    // serde->node = node;
+    SERDE(ar, foo, Name);
+    SERDE(ar, foo, Age);
+    SERDE(ar, foo, Height);
+    SERDE(ar, foo, Ints);
+    SERDE(ar, foo, Bars);
 }
 
 }  // namespace kdk
@@ -159,6 +176,10 @@ int main() {
     foo.Name = String("Example Foo");
     foo.Age = 25;
     foo.Height = 1.75f;
+    foo.Ints = NewDynArray<int>(&arena);
+    foo.Ints.Push(&arena, 1);
+    foo.Ints.Push(&arena, 2);
+    foo.Ints.Push(&arena, 3);
 
     // Add some bars
     Bar bar1;
@@ -166,16 +187,20 @@ int main() {
     bar1.Transform.Position = {1.0f, 2.0f, 3.0f};
     bar1.Transform.Rotation = {0.0f, 0.0f, 0.0f, 1.0f};
     bar1.Transform.Scale = 1.0f;
-    bar1.Ints = NewDynArray<int>(&arena);
-    bar1.Ints.Push(&arena, 1);
-    bar1.Ints.Push(&arena, 2);
-    bar1.Ints.Push(&arena, 3);
     bar1.Addresses = NewDynArray<String>(&arena);
     bar1.Addresses.Push(&arena, String("Address 1"));
-    bar1.Addresses.Push(&arena, String("Address 2"));
+    bar1.Addresses.Push(&arena, String(R"(This is a long string
+that spans multiple lines.
+It preserves newlines and special characters.)"));
+    bar1.Positions.Push(&arena, Vec3(1, 0, 0));
+    bar1.Positions.Push(&arena, Vec3(0, 1, 0));
+    bar1.Positions.Push(&arena, Vec3(0, 0, 1));
 
-    foo.Bars = NewDynArray<Bar>(&arena);
+    Bar bar2 = bar1;
+    bar2.Name = String("Bar Two");
+
     foo.Bars.Push(&arena, bar1);
+    foo.Bars.Push(&arena, bar2);
 
     Serde(&ar, "Foo", foo);
 
@@ -183,7 +208,5 @@ int main() {
 
     // // Save to file
     // SaveToFile(serde, "example.yaml");
-
-    printf("Serialization complete! Check example.yaml\n");
     return 0;
 }
