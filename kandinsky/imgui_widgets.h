3d9cc4fda9bf726f29a2525fd39b41938d3cc0e4
Cristián Donoso <cristiandonosoc@gmail.com>
2025-09-21 19:25:00 -0400

    [imgui] Little helper to create combo box from enums

--- kandinsky/imgui_widgets.h
+++ kandinsky/imgui_widgets.h
@@ -12,4 +12,20 @@ void BuildImGui_EntitySignature(EntitySignature signature);
 bool ImGui_WarningButton(const char* label, const ImVec2& size = ImVec2(0, 0));
 bool ImGui_DangerButton(const char* label, const ImVec2& size = ImVec2(0, 0));
 
+template <typename T>
+    requires std::is_enum_v<T>
+T ImGui_EnumCombo(String label, T current_value) {
+    FixedArray<const char*, (i32)T::COUNT> values;
+    for (i32 i = 0; i < (i32)T::COUNT; i++) {
+        T value = (T)i;
+        values.Push(ToString(value));
+    }
+
+    // We subtract 1 because we don't want to show the "Invalid" option.
+    int selected_index = (i32)current_value;
+    ImGui::Combo(label.Str(), &selected_index, values.Data, values.Size);
+
+    return (T)selected_index;
+}
+
 }  // namespace kdk
