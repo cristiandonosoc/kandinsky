53b07127788112870f5a72ad2117357b59b959e3
Cristián Donoso <cristiandonosoc@gmail.com>
2025-10-08 19:59:48 -0400

    [build] Use prebuilt yaml-cpp

---
+++ third_party/yaml-cpp-0.8.0/build_prebuild.bat
@@ -0,0 +1,10 @@
+@set SCRIPT_PATH=%~dp0
+@pushd %SCRIPT_PATH%\..\..
+
+@set YAML_PATH=third_party/yaml-cpp-0.8.0
+
+del /F/Q third_party\yaml-cpp-0.8.0\bin\yaml-cpp.lib
+bazel build %YAML_PATH%:yaml-cpp
+cp bazel-bin\%YAML_PATH%\yaml-cpp.lib %YAML_PATH%\bin\yaml-cpp.lib
+
+@popd
